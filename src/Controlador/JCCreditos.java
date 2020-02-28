5ea7e1c4f33a93972d429aca725b3dbf61d98829
frans <frans@192.168.1.70>
2020-02-28 11:23:43 -0600

    credito

--- src/Controlador/JCCreditos.java
+++ src/Controlador/JCCreditos.java
@@ -1,14 +1,26 @@
 package Controlador;
 
+import Vista.JDCompilador;
 import Vista.JDCreditos;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class JCCreditos {
+public class JCCreditos implements ActionListener {
 
     JDCreditos c = new JDCreditos(null, true);
 
     public JCCreditos(JDCreditos creditos) {
         this.c = creditos;
-
+        c.btnCerrar.addActionListener(this);
     }
 
+    public void actionPerformed(ActionEvent a) {
+        if (c.btnCerrar == a.getSource()) {
+            c.dispose();
+            JDCompilador com = new JDCompilador(null, true);
+            com.setLocationRelativeTo(null);
+            JCCompilador nuevo = new JCCompilador(com);
+            com.setVisible(true);
+        }
+    }
 }
