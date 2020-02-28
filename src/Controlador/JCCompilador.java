15ad704f6667aae12161ffcd31cfe894eb15a667
ashle <ashle@Ash>
2020-02-28 16:13:37 -0600

    arreglo

--- src/Controlador/JCCompilador.java
+++ src/Controlador/JCCompilador.java
@@ -7,17 +7,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.table.DefaultTableModel;
 
 public class JCCompilador implements ActionListener, KeyListener {
 
     JDCompilador c = new JDCompilador(null, true);
-    Compilador comp = new Compilador();
+    Compilador comp = new Compilador(c);
+    DefaultTableModel modelo = new DefaultTableModel();
 
     public JCCompilador(JDCompilador compilador) {
         this.c = compilador;
         this.c.txtIngreso.addKeyListener(this);
         this.c.btnCreditos.addActionListener(this);
         this.c.btnSalir.addActionListener(this);
+        this.c.jtfRespuesta.addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent a) {
@@ -45,7 +48,7 @@ public class JCCompilador implements ActionListener, KeyListener {
         if (e.getKeyCode() == KeyEvent.VK_ENTER) {
             String operacion;
             operacion = this.c.txtIngreso.getText();
-            c.jtfRespuesta.setText(comp.respu(operacion));
+            this.comp.respu(operacion);
         }
     }
 
