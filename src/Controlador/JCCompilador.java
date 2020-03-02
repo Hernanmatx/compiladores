328e1a48938bab47e961a5d461767a17ab9c57f4
ashle <ashle@Ash>
2020-03-02 11:17:37 -0600

    Cambios

--- src/Controlador/JCCompilador.java
+++ src/Controlador/JCCompilador.java
@@ -7,20 +7,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
 
 public class JCCompilador implements ActionListener, KeyListener {
 
     JDCompilador c = new JDCompilador(null, true);
-    Compilador comp = new Compilador(c);
+    Compilador comp = new Compilador();
     DefaultTableModel modelo = new DefaultTableModel();
-
+    private int contador = 0, cont = 1;
+    
     public JCCompilador(JDCompilador compilador) {
         this.c = compilador;
         this.c.txtIngreso.addKeyListener(this);
         this.c.btnCreditos.addActionListener(this);
         this.c.btnSalir.addActionListener(this);
         this.c.jtfRespuesta.addActionListener(this);
+        this.c.jtfRespuesta.setEditable(false);
+        this.c.jTable1.setEnabled(false);
     }
 
     public void actionPerformed(ActionEvent a) {
@@ -50,7 +54,30 @@ public class JCCompilador implements ActionListener, KeyListener {
             operacion = this.c.txtIngreso.getText();
             opr = this.comp.respu(operacion);
             this.c.jtfRespuesta.setText(opr);
-            
+            datos();
         }
+    }
+
+    public void datos() {
+        cont = 1;
+        this.modelo.setRowCount(0);
+        ArrayList<Object> num = new ArrayList<>();
+        num = (ArrayList<Object>) this.comp.getNum();
+        ArrayList<Object> ide = new ArrayList<>();
+        ide = (ArrayList<Object>) this.comp.getIde();
+        Object[] numero = new Object[num.size()];
+        Object[] identi = new Object[ide.size()];
+        for (int i = 0; i < num.size(); i++) {
+            numero[i] = num.get(i);
+        }
+        for (int i = 0; i < ide.size(); i++) {
+            identi[i] = ide.get(i);
+        }
+        modelo.setColumnIdentifiers(new Object[]{"Número", "Token", "Identificador"});
+        while(contador < num.size()) {
+            modelo.addRow(new Object[]{cont++, numero[contador], identi[contador]});
+            contador++;
+        }
+        this.c.jTable1.setModel(modelo);
     }
 }
